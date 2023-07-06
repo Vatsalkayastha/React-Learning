0d539df5c13f797c8b1f1dabf7b5e1efd07e39d1
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2023-07-06 10:57:04 +0530

    first commit

---
+++ Todo-List/src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import Contactlist from './Contactlist';
+
+function App() {
+  return(
+    <div>
+      <Header />
+      {/* <AddContact />
+      <Contactlist /> */}
+    </div>
+  );
+}
+
+export default App;
